ea8dcdaa91f8c1867c37161037c9bfeb85f79304
jeetachacko <jeetachacko@gmail.com>
2023-08-19 21:05:56 +0200

    Minor changes

--- scripts/scf_run.sh
+++ scripts/scf_run.sh
@@ -3,7 +3,7 @@
 # !TODO! Update hardcoded values
 
 chaincode=generator
-tps=(50 100 200 400 800)
+tps=(50 100 150 200 250 300)
 
 cd /home/ubuntu/hll3_opennebula
 
@@ -34,7 +34,23 @@ while true; do
     ./scripts/caliper_delete.sh
     #selectedtps=${tps[ $RANDOM % ${#tps[@]} ]}
     #sed -i "s/.*tps.*/            tps: $selectedtps/" /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config.yaml
-    ./scripts/caliper_run.sh $chaincode
+
+    timeout 360s ./scripts/caliper_run.sh $chaincode ; if [ $? -eq 124 ] ; then succ=0 ; else succ=$(printf "%i" $(grep '| common |' /home/ubuntu/hll3_opennebula/caliper/caliper-logs.txt | awk '{print $4}' | tail -n 1)) ; fi
+    #succ=$(printf "%i" $(grep '| common |' /home/ubuntu/hll3_opennebula/caliper/caliper-logs.txt | awk '{print $4}' | tail -n 1))
+    if [ $succ == 0 ]; then
+        ./scripts/caliper_delete.sh
+        sleep 30s
+        timeout 360s ./scripts/caliper_run.sh $chaincode ; if [ $? -eq 124 ] ; then succ=0 ; else succ=$(printf "%i" $(grep '| common |' /home/ubuntu/hll3_opennebula/caliper/caliper-logs.txt | awk '{print $4}' | tail -n 1)) ; fi
+    fi
+
+    if [ $succ == 0 ]; then
+        ./scripts/network_delete.sh
+        ./scripts/caliper_delete.sh
+        sleep 30s
+        ./scripts/network_run.sh
+        sleep 10s
+        timeout 360s ./scripts/caliper_run.sh $chaincode ; if [ $? -eq 124 ] ; then succ=0 ; else succ=$(printf "%i" $(grep '| common |' /home/ubuntu/hll3_opennebula/caliper/caliper-logs.txt | awk '{print $4}' | tail -n 1)) ; fi
+    fi
     sed -i 1,200d /home/ubuntu/hll3_opennebula/caliper/caliper-logs.txt
     if [ $count == 1 ]; then
         echo "Delete check file"
