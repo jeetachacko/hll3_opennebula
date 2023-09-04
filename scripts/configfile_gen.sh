e5ae3a0aaccd9b677a7cc393b71a6ea861213300
jeetachacko <jeetachacko@gmail.com>
2023-09-04 11:20:49 +0200

    Scaled network size

--- scripts/configfile_gen.sh
+++ scripts/configfile_gen.sh
@@ -2,10 +2,12 @@
 
 # !TODO! Update hardcoded values
 
+#Multiple tps - Dynamic Workload
+
 chaincode=generator
-tps=(10 50 100 500 1000)
-num_entries=20
-num_entries_per_tps=18
+tps=(100 500 800)
+num_entries=6
+num_entries_per_tps=60
 
 cp /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config_gen.yaml /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config.yaml
 
@@ -18,11 +20,15 @@ for i in $(seq 1 ${num_entries}); do
     done
 done
 
-#    - label: common
-#      txDuration: 10
-#      rateControl:
-#          type: fixed-rate
-#          opts:
-#            tps: 800
-#      workload:
-#        module: /caliper-workload/common.js
+#Single tps - Uniform Workload
+# chaincode=generator
+# tps=500
+# num_entries=360
+
+# cp /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config_gen.yaml /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config.yaml
+
+# for i in $(seq 1 ${num_entries}); do
+#         configlines=( "    - label: common" "      txDuration: 10" "      rateControl:" "          type: fixed-rate" "          opts:" "            tps: ${tps}" "      workload:" "        module: /caliper-workload/common.js" );
+#         printf '\n' >> /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config.yaml;
+#         printf '\n%s' "${configlines[@]}" >> /home/ubuntu/hll3_opennebula/caliper/benchmarks/$chaincode/config.yaml;
+# done
