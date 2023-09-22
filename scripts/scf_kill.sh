8bd027e761f6ad6b3fea60b104977cba97f554d5
jeetachacko <jeetachacko@gmail.com>
2023-09-22 14:45:50 +0200

    Script for RL step triggered tps change

--- scripts/scf_kill.sh
+++ scripts/scf_kill.sh
@@ -11,6 +11,8 @@ pkill -9 -f ./scripts/k8s-updateconfig.sh
 pkill -9 -f wandb-service
 pkill -9 -f ./scripts/monitor.sh
 rm /home/ubuntu/hll3_opennebula/check.txt
+rm /home/ubuntu/hll3_opennebula/tpsupdate.txt
+rm /home/ubuntu/hll3_opennebula/rlupdate.txt
 pkill -9 -f ./scripts/scf_run.sh
 pkill -9 -f ./scripts/network_run.sh
 pkill -9 -f ./scripts/configfile_gen.sh
